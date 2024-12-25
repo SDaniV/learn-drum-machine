a9a001e49c9b86d6def84ecd9d10001822f6397d
SDaniV <sorokadanylo1@gmail.com>
2024-12-25 13:18:10 +0200

    Small refactoring of App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,24 @@ import './App.css';
 
 import React, { useEffect, useState } from 'react';
 
-function App(props) {
+const App = (props) => {
   const [recentSample, setRecentSample] = useState('');
+
+  const soundButtons = [
+    { id: 'Heater-1', key: 'Q', src: './audio-samples/Heater-1.mp3' },
+    { id: 'Heater-2', key: 'W', src: './audio-samples/Heater-2.mp3' },
+    { id: 'Heater-3', key: 'E', src: './audio-samples/Heater-3.mp3' },
+    { id: 'Heater-4', key: 'A', src: './audio-samples/Heater-4_1.mp3' },
+    { id: 'Clap', key: 'S', src: './audio-samples/Heater-6.mp3' },
+    { id: 'Open-HH', key: 'D', src: './audio-samples/Dsc_Oh.mp3' },
+    { id: "Kick-n'-Hat", key: 'Z', src: './audio-samples/Kick_n_Hat.mp3' },
+    { id: 'Kick', key: 'X', src: './audio-samples/RP4_KICK_1.mp3' },
+    { id: 'Closed-HH', key: 'C', src: './audio-samples/Cev_H2.mp3' }
+  ];
 
   const playAudio = (sample) => {
     setRecentSample(sample);
-    var playPromise = document.getElementById(sample).play();
+    let playPromise = document.getElementById(sample).play();
 
     if (playPromise !== undefined) {
       playPromise.then(_ => {
@@ -23,25 +35,9 @@ function App(props) {
     }
   };
 
-  const handleClick = (sample) => {
-    playAudio(sample) // Or any other action
-  };
-
-  const soundButton = [
-    { id: 'Heater-1', key: 'Q', src: './audio-samples/Heater-1.mp3' },
-    { id: 'Heater-2', key: 'W', src: './audio-samples/Heater-2.mp3' },
-    { id: 'Heater-3', key: 'E', src: './audio-samples/Heater-3.mp3' },
-    { id: 'Heater-4', key: 'A', src: './audio-samples/Heater-4_1.mp3' },
-    { id: 'Clap', key: 'S', src: './audio-samples/Heater-6.mp3' },
-    { id: 'Open-HH', key: 'D', src: './audio-samples/Dsc_Oh.mp3' },
-    { id: "Kick-n'-Hat", key: 'Z', src: './audio-samples/Kick_n_Hat.mp3' },
-    { id: 'Kick', key: 'X', src: './audio-samples/RP4_KICK_1.mp3' },
-    { id: 'Closed-HH', key: 'C', src: './audio-samples/Cev_H2.mp3' }
-  ];
-
   useEffect(() => {
     const handleKeyDown = (e) => {
-      soundButton.forEach(button => {
+      soundButtons.forEach(button => {
         if (button.key === e.key) {
           const element = document.getElementById(button.id);
           if (element) {
@@ -71,32 +67,31 @@ function App(props) {
             W
           </div>
           <div className="drum-pad" id="Heater-3" onClick={() => playAudio("E")}>
-            <audio className="clip" id="E" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3"></audio>
+            <audio className="clip" id="E" src="./audio-samples/Heater-3.mp3"></audio>
             E
           </div>
           <div className="drum-pad" id="Heater-4" onClick={() => playAudio("A")}>
-            <audio className="clip" id="A" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3"></audio>
+            <audio className="clip" id="A" src="./audio-samples/Heater-4_1.mp3"></audio>
             A
           </div>
           <div className="drum-pad" id="Clap" onClick={() => playAudio("S")}>
-            <audio className="clip" id="S" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"></audio>
+            <audio className="clip" id="S" src="./audio-samples/Heater-6.mp3"></audio>
             S
           </div>
           <div className="drum-pad" id="Open-HH" onClick={() => playAudio("D")}>
-            <audio className="clip" id="D" src="https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3"></audio>
+            <audio className="clip" id="D" src="./audio-samples/Dsc_Oh.mp3"></audio>
             D
           </div>
           <div className="drum-pad" id="Kick-n'-Hat" onClick={() => playAudio("Z")}>
-            {/* <audio className="clip" id="Z" src="https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3"></audio> */}
             <audio className="clip" id="Z" src="./audio-samples/Kick_n_Hat.mp3"></audio>
             Z
           </div>
           <div className="drum-pad" id="Kick" onClick={() => playAudio("X")}>
-            <audio className="clip" id="X" src="https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3"></audio>
+            <audio className="clip" id="X" src="./audio-samples/RP4_KICK_1.mp3"></audio>
             X
           </div>
           <div className="drum-pad" id="Closed-HH" onClick={() => playAudio("C")}>
-            <audio className="clip" id="C" src="https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"></audio>
+            <audio className="clip" id="C" src="./audio-samples/Cev_H2.mp3"></audio>
             C
           </div>
         </div>
